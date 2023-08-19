a4ce3f1f78a219b84387e041b0ccf5bb319b35d1
kimhyunjo <alsxmqkqh23@gmail.com>
2023-08-20 04:58:04 +0900

    :pencil2: Fix type

--- src/constant/URL.ts
+++ src/constant/URL.ts
@@ -2,7 +2,7 @@ const URL = {
   MAIN: '/',
   PRODUCT: '/products',
   REVIEW_MANAGE: '/review-manage',
-  PRODUCT_REVIEW: '/products/:productId/reviews'
+  PRODUCT_REVIEW: '/products/:productId/reviews',
 };
 
 export default URL;
